8d5941f145e34fbc11645031f3ad104400dc97d9
tgrowden <taylorgrowden@gmail.com>
2016-04-03 19:48:59 -0400

    updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 $(function() {
   var flask = new CodeFlask;
-  flask.run('#js-wrapper', {
-    language: 'javascript'
+  var flaskLang = '';
+  flask.run('#code-wrapper', {language: flaskLang});
+  flask.onUpdate(function(code) {
+    console.log("User's input code: " + code);
+  });
+  $('#languageSelect').on('change', function() {
+    var flaskLang = this.value;
+    var foo = $('#code-wrapper code').text();
+    flask.run('#code-wrapper', {language: flaskLang});
+    flask.update(foo);
+  });
+  $('#themeSelect').on('change', function() {
+    var theme = this.value;
+    if (theme == 'default') {
+      $('#prism-css').html('<link rel="stylesheet" href="/css/themes/prism.css" />');
+    } else {
+      $('#prism-css').html('<link rel="stylesheet" href="/css/themes/prism-' + theme + '.css" />');
+    }
   });
 });
