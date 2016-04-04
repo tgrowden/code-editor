e6d0b669e075739883ed475c1a84aa07c35cea29
tgrowden <taylorgrowden@gmail.com>
2016-04-04 16:05:58 -0400

    add logic to try and change language

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,10 @@
 $(function() {
-  var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-    lineNumbers: true,
-    language: 'javascript'
-  });
+  function setLang(lang) {
+    $('#lang').html('<script src="/components/codemirror/mode/' + lang + '/' + lang + '.js"></script>');
+    var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+      lineNumbers: true,
+      language: 'javascript'
+    });
+  }
+//  setLang('javascript');
 });
