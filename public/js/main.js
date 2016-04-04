4a64110d1ee0a9c48a7f4a0bf4e6d3268c588d0d
tgrowden <taylorgrowden@gmail.com>
2016-04-04 15:14:27 -0400

    use codemirror instead of codeflask/prism

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,6 @@
 $(function() {
-  var flask = new CodeFlask;
-  var flaskLang = '';
-  flask.run('#code-wrapper', {language: flaskLang});
-  flask.onUpdate(function(code) {
-    console.log("User's input code: " + code);
-  });
-  $('#languageSelect').on('change', function() {
-    var flaskLang = this.value;
-    var foo = $('#code-wrapper code').text();
-    flask.run('#code-wrapper', {language: flaskLang});
-    flask.update(foo);
-  });
-  $('#themeSelect').on('change', function() {
-    var theme = this.value;
-    if (theme == 'default') {
-      $('#prism-css').html('<link rel="stylesheet" href="/css/themes/prism.css" />');
-    } else {
-      $('#prism-css').html('<link rel="stylesheet" href="/css/themes/prism-' + theme + '.css" />');
-    }
+  var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    lineNumbers: true,
+    language: 'javascript'
   });
 });
