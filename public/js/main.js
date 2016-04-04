92da05c056947204ddcbc2b1b87da35dec4a7b7f
tgrowden <taylorgrowden@gmail.com>
2016-04-04 16:56:20 -0400

    add function to change language

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 $(function() {
-  function setLang(lang) {
+  window.editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+    lineNumbers: true
+  });
+  window.setLang = function(lang) {
     $('#lang').html('<script src="/components/codemirror/mode/' + lang + '/' + lang + '.js"></script>');
-    var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-      lineNumbers: true,
-      language: 'javascript'
-    });
-  }
-//  setLang('javascript');
+    editor.setOption("mode", lang);
+  };
+  setLang('javascript');
 });
