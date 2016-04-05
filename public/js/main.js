56af88f3d70c15083e67f6ae13d635ab7c6096ba
tgrowden <taylorgrowden@gmail.com>
2016-04-05 15:46:56 -0400

    add frontend updates

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ $(function() {
     lineNumbers: true,
     tabSize: 2
   });
+  window.editor = editor;
   var setLang = function(lang) {
     $('#lang').html('<script src="/components/codemirror/mode/' + lang + '/' + lang + '.js"></script>');
     editor.setOption("mode", lang);
@@ -23,6 +24,22 @@ $(function() {
   $('#themeSelect').change(function() {
     setTheme($(this).val());
   });
+  // handle changing Options ====================
+  var toggleOption = function(opt) {
+    if (editor.getOption(opt) === true) {
+      editor.setOption(opt, false);
+    } else if (editor.getOption(opt) === false) {
+      editor.setOption(opt, true);
+    }
+  };
+  $('li.codeOptions').click(function() {
+    $(this).toggleClass('active');
+    if ($(this).attr('data-type') == "boolean") {
+      var opt = $(this).attr('data-value');
+      toggleOption(opt);
+    }
+  });
+  //end option handling =========================
   // Web Sockets
   var socket = io();
   socket.on('connection', function(data) {
